8e3c0a1446b84676c14375e3ba63e73fb6abe538
Yann Grallan <grallanyann84@gmail.com>
2026-02-18 13:50:57 +0100

    Delete Secondary_Client.bat

--- Secondary_Client.bat
+++
@@ -1,5 +0,0 @@
-SET "UE_EXE=C:\Program Files\Epic Games\UE_5.7\Engine\Binaries\Win64\UnrealEditor.exe"
-
-SET "PROJECT=C:\Repos\Unreal Projects\TD6_Backroom\TD6_Backroom.uproject"
-
-start "" "%UE_EXE%" "%PROJECT%" -game -windowed -ResX=1200 -ResY=900 -AUTH_TYPE=Developer -AUTH_LOGIN=127.0.0.1:8082 -AUTH_PASSWORD=Charles_Testing -log
