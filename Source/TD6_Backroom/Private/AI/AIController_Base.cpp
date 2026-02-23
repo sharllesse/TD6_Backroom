53ae0a6fbd378fff1c3733350e77066a6a17882c
Charles Lesage <charles19lesage06@gmail.com>
2026-02-23 11:44:50 +0100

    Add base ai class

---
+++ Source/TD6_Backroom/Private/AI/AIController_Base.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/AIController_Base.h"
+
+
+// Sets default values
+AAIController_Base::AAIController_Base()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAIController_Base::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAIController_Base::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
